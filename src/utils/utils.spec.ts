9e1b0dfeb45c3953269292fd63c972fba8654f3a
Johannes Klose <jklose@calitrix.com>
2022-07-22 14:06:43 +0200

    refactor: support case conversion in column utils

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,4 +1,6 @@
+import { camelCase, makeTransform, snakeCase } from '../mapper'
 import * as RT from '../runtypes'
+import { makeSql } from '../sql'
 
 import { columns } from './utils'
 
@@ -25,6 +27,37 @@ describe('utils', () => {
           RT.Aliased('t', RT.Record({ b: RT.Number }))
         ).toQuery().text
       ).toBe('"a", "t"."b" AS "t__b"')
+    })
+
+    it('should use case transforms if provided', () => {
+      const config = {
+        caseTransform: makeTransform(camelCase, snakeCase),
+      }
+      const sql = makeSql(config)
+
+      expect(
+        sql`${columns(
+          RT.Record({ someCol: RT.Number, otherCol: RT.Number })
+        )}`.toQuery().text
+      ).toBe('"some_col", "other_col"')
+    })
+
+    it('should use case transforms with aliases if provided', () => {
+      const config = {
+        caseTransform: makeTransform(camelCase, snakeCase),
+      }
+      const sql = makeSql(config)
+
+      expect(
+        sql`${columns(
+          RT.Aliased(
+            'tab',
+            RT.Record({ someCol: RT.Number, otherCol: RT.Number })
+          )
+        )}`.toQuery().text
+      ).toBe(
+        '"tab"."some_col" AS "tab__some_col", "tab"."other_col" AS "tab__other_col"'
+      )
     })
   })
 })
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,34 +1,48 @@
+import { fieldToColumnName } from '../mapper/mapper'
 import {
   getDescribedFields,
   isBrand,
   MaybeBrandedRecord,
 } from '../mapper/reflect'
-import { CallableQuery, FunctionalQuery, sql } from '../sql'
+import { CallableQuery, createCallableQuery, CreatePartsFunction } from '../sql'
 
 // https://github.com/brianc/node-postgres/blob/master/lib/client.js#L385
 const escapeIdentifier = (str: string) => `"${str.replace(/"/g, '""')}"`
 
 export const columns = (...types: MaybeBrandedRecord[]): CallableQuery<[]> => {
-  const columns: string[] = []
+  const getParts: CreatePartsFunction<[]> = (config) => {
+    const columns: string[] = []
 
-  for (const type of types) {
-    const alias = isBrand(type.reflect) ? type.reflect.brand : null
-    const fields = getDescribedFields(type.reflect)
+    for (const type of types) {
+      const alias = isBrand(type.reflect) ? type.reflect.brand : null
+      const fields = getDescribedFields(type.reflect)
 
-    if (alias == null) {
-      for (const fieldName of Object.keys(fields)) {
-        columns.push(escapeIdentifier(fieldName))
-      }
-    } else {
-      for (const fieldName of Object.keys(fields)) {
-        columns.push(
-          `${escapeIdentifier(alias)}.${escapeIdentifier(
-            fieldName
-          )} AS ${escapeIdentifier(`${alias}__${fieldName}`)}`
-        )
+      if (alias == null) {
+        for (const fieldName of Object.keys(fields)) {
+          columns.push(
+            escapeIdentifier(
+              fieldToColumnName(fieldName, undefined, config.caseTransform)
+            )
+          )
+        }
+      } else {
+        for (const fieldName of Object.keys(fields)) {
+          const nameInRow = fieldToColumnName(
+            fieldName,
+            undefined,
+            config.caseTransform
+          )
+          columns.push(
+            `${escapeIdentifier(alias)}.${escapeIdentifier(
+              nameInRow
+            )} AS ${escapeIdentifier(`${alias}__${nameInRow}`)}`
+          )
+        }
       }
     }
+
+    return { texts: [columns.join(', ')], values: [] }
   }
 
-  return new CallableQuery([columns.join(', ')], [])
+  return createCallableQuery(getParts, {})
 }
